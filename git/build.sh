73ab639b7f05cf15456d18f946f1713884fc78b6
wentang <wentang.zhou@outlook.com>
2019-06-16 06:56:26 +0000

    Add git build script

---
+++ git/build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BUILD_PATH=${SCRIPT_PATH}/build
+LOG_FILE=${BUILD_PATH}/error.txt
+cd ${SCRIPT_PATH}
+
+function log {
+    echo "$1" >> ${LOG_FILE}
+}
+
+# $1: retcode
+# $2: error message
+function check_result {
+    if [ "$1" != "0" ]; then
+        log "$2"
+        exit $1
+    fi
+}
+
+VER=2.22.0
+
+# decompress source
+SRC_PATH=${SCRIPT_PATH}/git-${VER}
+if [ -d ${SRC_PATH} ]; then
+    rm -r ${SRC_PATH}
+fi
+tar xvf git-${VER}.tar.gz
+cd ${SRC_PATH}
+
+# bootstrap
+log "-- configure"
+make configure
+${SRC_PATH}/configure --prefix=/usr
+result=$?
+check_result ${result} "-- configure failed"
+
+# make
+log ""
+log "-- make"
+make -j4 2>>${LOG_FILE}
+result=$?
+check_result ${result} "-- make failed"
+
+# install
+log ""
+log "-- install"
+sudo make install 2>>${LOG_FILE}
+result=$?
+check_result ${result} "-- install failed"
+
+# auto completion
+cp contrib/completion/git-completion.bash ~/.git-completion.bash
+echo source ~/.git-completion.bash >> ~/.bashrc
